51ec52eec35c0ac0da8cc077e4bbb428700b01ad
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-10 21:38:42 -0300

    New changes

--- testes.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-opcoesPossiveis = (
-            'Para destravar a porta, escolha a senha:\nrES703r4d4[1]     teRr2sATurn [2]',
-            'Aperte o botão CORRETO para usar o rádio:\nMHZ-GMArconi [1]     StrongBye[2]', 
-            'Redirecione a nave para enviar o sinal para a outra missão:\n194°FahrRadians[1]     100N°toStone[2]'  
-        )
-
-print(opcoesPossiveis[1])
